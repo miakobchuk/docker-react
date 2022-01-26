fa64823e5e7da74eec36e78b49205b042d1d9530
miakobchuk <iakobchu@adobe.com>
2022-01-25 20:03:01 -0600

    added aws config v4

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Added by Maks v14!
+          Added by Maks v15!
         </p>
         <a
           className="App-link"
